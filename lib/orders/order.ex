b998ed3a7146d120811ac7d74397104c1b786504
hitk1 <luispaulo.degini@gmail.com>
2022-05-26 23:17:50 -0300

    feat: logica de build dos pedidos

--- lib/orders/order.ex
+++ lib/orders/order.ex
@@ -1,16 +1,34 @@
 defmodule Exlivery.Orders.Order do
+  alias Exlivery.Orders.Item
+  alias Exlivery.Users.User
+
   @attributes [:user_cpf, :delivery_address, :items, :total_price]
-  @enforce_attributes @keys
+  @enforce_keys @attributes
 
   defstruct @attributes
 
-  def build do
+  def build(
+        %User{cpf: cpf, address: address},
+        [%Item{} | _sitems] = items
+      ) do
     {:ok,
      %__MODULE__{
-       user_cpf: nil,
-       delivery_address: nil,
+       user_cpf: cpf,
+       delivery_address: address,
        items: nil,
-       total_price: nil
+       total_price: calculate_total(items)
      }}
+  end
+
+  def build(_users, _items), do: {:error, "Invalid parameters"}
+
+  defp calculate_total(items) do
+    Enum.reduce(items, Decimal.new("0.00"), &sum_prices/2)
+  end
+
+  defp sum_prices(%Item{unit_price: unit_price, quantity: quantity}, acc) do
+    unit_price
+    |> Decimal.mult(quantity)
+    |> Decimal.add(acc)
   end
 end
--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,5 +1,5 @@
 defmodule Exlivery.Users.User do
-  @attributes [:name, :email, :cpf, :age]
+  @attributes [:address, :name, :email, :cpf, :age]
   # a diretiva "enforce keys", diz que a estrutura passada como parametro, tem atributos obrigatórios
   # por sua vez, quando definida na 'struct', a mesma não poderá ser inicializada com os respectivos valores em nil
   @enforce_keys @attributes
@@ -8,9 +8,10 @@ defmodule Exlivery.Users.User do
 
   # é comum ter uma função para buildar a estrutura, para faciltar
   # No caso só sera possivel criar a struct se age for maior ou igual a 18
-  def build(name, email, cpf, age) when age >= 18 do
+  def build(address, name, email, cpf, age) when age >= 18 do
     {:ok,
      %__MODULE__{
+       address: address,
        name: name,
        email: email,
        cpf: cpf,
@@ -18,5 +19,5 @@ defmodule Exlivery.Users.User do
      }}
   end
 
-  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
+  def build(_address, _name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
 end
