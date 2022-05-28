b208543041712e859347353065928d7a55ef0926
hitk1 <luispaulo.degini@gmail.com>
2022-05-28 17:10:27 -0300

    feat: setup e configuracao do padrao 'facade'

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,5 +1,15 @@
 defmodule Exlivery do
-  def hello do
-    :world
+  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  def start_agents do
+    UserAgent.start_link(%{})
   end
+
+  # As notações de 'delegate' são do padrão 'facade'
+  # Onde somente este modulo fica como 'interface publica' e ele delega para submodulos da arquitetura
+  # quais funções executar
+  # No caso, é definido uma funcao 'create_or_update' que recebe alguns parametros, que por sua vez
+  # executa a função 'call' dentro do modulo 'CreateOrUpdate'
+  defdelegate create_or_update(params), to: CreateOrUpdate, as: :call
 end
